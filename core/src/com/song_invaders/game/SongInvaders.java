ff289f5f71ed4c545bc2ece869a596cf80902238
Nathan Cooper <nac33@students.uwf.edu>
2017-09-23 15:49:09 -0500

    Load sound files

--- core/src/com/song_invaders/game/SongInvaders.java
+++ core/src/com/song_invaders/game/SongInvaders.java
@@ -3,6 +3,9 @@ package com.song_invaders.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,12 +14,21 @@ import com.song_invaders.game.screens.PlayScreen;
 public class SongInvaders extends Game
 {
 	public SpriteBatch batch;
+    public static AssetManager manager;
     public static final int WIDTH = 800;
     public static final int HEIGHT = 480;
 
 	@Override
 	public void create () {
 		this.batch = new SpriteBatch();
+        manager = new AssetManager();
+
+        manager.load("audio/sounds/SC/SC/SC_1.wav", Sound.class);
+        manager.load("audio/sounds/SC/SC/SC_2.wav", Sound.class);
+        manager.load("audio/sounds/SC/SC/SC_3.wav", Sound.class);
+        manager.load("audio/sounds/SC/SC/SC_4.wav", Sound.class);
+        manager.load("audio/sounds/SC/SC/SC_full.wav", Music.class);
+        manager.finishLoading();
 
         this.setScreen(new PlayScreen(this));
 	}
