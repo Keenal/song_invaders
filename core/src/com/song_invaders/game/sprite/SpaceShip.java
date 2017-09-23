5fc5a7d5168269561a41a7c4f7efe1bf14d41672
Howtoon <Yomamasucksmy380>
2017-09-23 13:31:03 -0500

    added triangle to spaceship

--- core/src/com/song_invaders/game/sprite/SpaceShip.java
+++ core/src/com/song_invaders/game/sprite/SpaceShip.java
@@ -1,10 +1,13 @@
 package com.song_invaders.game.sprite;
 
+
+import com.badlogic.gdx.graphics.g3d.particles.values.MeshSpawnShapeValue;
+
 /**
  * Created by Keenal on 9/23/2017.
  */
 
 public class SpaceShip {
 
-    boolean isGood;
+    MeshSpawnShapeValue.Triangle shape;
 }
