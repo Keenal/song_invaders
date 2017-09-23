59addbe4e35512d4d1087211fa8454d3ed1f0f8f
Howtoon <Yomamasucksmy380>
2017-09-23 13:26:01 -0500

    Added MShip

---
+++ core/src/com/song_invaders/game/sprite/MShip.java
@@ -0,0 +1,25 @@
+package com.song_invaders.game.sprite;
+
+/**
+ * Created by Howtoon on 9/23/17.
+ */
+
+public class MShip {
+    Target[] targets;
+    Column[] columns;
+
+    public MShip()
+    {
+
+    }
+
+    public void dropTarget()
+    {
+
+    }
+
+    public void update()
+    {
+
+    }
+}
