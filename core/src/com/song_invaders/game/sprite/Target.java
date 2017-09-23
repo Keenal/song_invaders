c8f2cbc2bb108c7bb05de9d03893bf6d5f905499
Howtoon <Yomamasucksmy380>
2017-09-23 14:54:45 -0500

    adding constructor to Target

--- core/src/com/song_invaders/game/sprite/Target.java
+++ core/src/com/song_invaders/game/sprite/Target.java
@@ -18,7 +18,51 @@ public class Target {
 
     public Target()
     {
+        img = new Texture("badlogic.jpg");
+        sprite = new Sprite(img);
 
+        // Center the sprite in the top/middle of the screen
+        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
+                Gdx.graphics.getHeight() / 2);
+
+        // Create a physics world, the heart of the simulation.  The Vector
+        passed in is gravity
+        world = new World(new Vector2(0, -98f), true);
+
+        // Now create a BodyDefinition.  This defines the physics objects type
+        and position in the simulation
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        // We are going to use 1 to 1 dimensions.  Meaning 1 in physics engine
+        is 1 pixel
+        // Set our body to the same position as our sprite
+        bodyDef.position.set(sprite.getX(), sprite.getY());
+
+        // Create a body in the world using our definition
+        body = world.createBody(bodyDef);
+
+        // Now define the dimensions of the physics shape
+        PolygonShape shape = new PolygonShape();
+        // We are a box, so this makes sense, no?
+        // Basically set the physics polygon to a box with the same dimensions
+        as our sprite
+        shape.setAsBox(sprite.getWidth()/2, sprite.getHeight()/2);
+
+        // FixtureDef is a confusing expression for physical properties
+        // Basically this is where you, in addition to defining the shape of the
+        body
+        // you also define it's properties like density, restitution and others
+        we will see shortly
+        // If you are wondering, density and area are used to calculate over all
+        mass
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1f;
+
+        Fixture fixture = body.createFixture(fixtureDef);
+
+        // Shape is the only disposable of the lot, so get rid of it
+        shape.dispose();
     }
 
     public void update()
