2ff4c3d188e47225444b57dd9b5c8ab3ca958ce6
keenal <kms118@students.uwf.edu>
2017-09-23 13:25:33 -0500

    added Column and TargetZone classes

---
+++ core/src/com/song_invaders/game/Sprite/Column.java
@@ -0,0 +1,30 @@
+package com.song_invaders.game.Sprite;
+
+/**
+ * Created by Keenal on 9/23/2017.
+ */
+
+public class Column {
+
+    int xValue;
+
+    public Column(int xValue) {
+        this.xValue = xValue;
+    }
+
+    public int getxValue() {
+        return xValue;
+    }
+
+    public void setxValue(int xValue) {
+        this.xValue = xValue;
+    }
+
+    public void update(){
+
+    }
+}
+
+
+
+
---
+++ core/src/com/song_invaders/game/sprite/TargetZone.java
@@ -0,0 +1,14 @@
+package com.song_invaders.game.sprite;
+
+/**
+ * Created by Keenal on 9/23/2017.
+ */
+
+public class TargetZone {
+
+    // position: variable?
+
+    public void update(){
+
+    }
+}
