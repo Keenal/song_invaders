1129773c939dec6f0c09756106c48f332da542d8
Nathan Cooper <nac33@students.uwf.edu>
2017-09-23 12:10:24 -0500

    Initial Commit

---
+++ desktop/src/com/song_invaders/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.song_invaders.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.song_invaders.game.Song Invaders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Song Invaders(), config);
+	}
+}
---
+++ html/src/com/song_invaders/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.song_invaders.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.song_invaders.game.Song Invaders;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Song Invaders();
+        }
+}
